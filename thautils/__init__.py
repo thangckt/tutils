cfed85c74f21b4f2f6351a0b0a5ef5cbd7312bae
Thang Nguyen <46436648+thangckt@users.noreply.github.com>
2024-06-27 17:23:46 +0900

    u

--- thautils/__init__.py
+++ thautils/__init__.py
@@ -1,3 +1,17 @@
+#
+"""
+<img src="./1images/bear_line.png" style="float: left; margin-right: 20px" width="120" />
+
+The package for general ulitities.
+
+This package is developed and maintained by [C.Thang Nguyen](https://thangckt.github.io)
+"""
 
 from .general_utils import *
 from .check_installation import *
+
+__version__ = "0.1"
+__description__ = "Python package"
+__long_description__ = "ML based applications "
+__author__ = "thangckt"
+__author_email__ = "thangckt@gmail.com"
