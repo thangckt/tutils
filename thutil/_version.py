232743fb0aa7fb0dc492c00ee5c062f9f359d4ec
Thang Nguyen <46436648+thangckt@users.noreply.github.com>
2024-10-10 09:29:08 +0900

    u

--- thutil/_version.py
+++ thutil/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev78+gfb97db2.d20240921'
-__version_tuple__ = version_tuple = (0, 1, 'dev78', 'gfb97db2.d20240921')
+__version__ = version = '0.1.dev87+g51ed9be.d20241010'
+__version_tuple__ = version_tuple = (0, 1, 'dev87', 'g51ed9be.d20241010')
---
+++ thutil/stuff.py
@@ -0,0 +1,7 @@
+from typing import Generator
+
+
+def chunk_list(input_list: list, n: int) -> Generator:
+    """Yield successive n-sized chunks from `input_list`."""
+    for i in range(0, len(input_list), n):
+        yield input_list[i : i + n]
