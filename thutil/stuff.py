18be68b154cf1f86ea3848a31ad6a06c6bea7ffd
Thang Nguyen <46436648+thangckt@users.noreply.github.com>
2024-10-20 22:14:21 +0900

    Update stuff.py

--- thutil/stuff.py
+++ thutil/stuff.py
@@ -14,7 +14,7 @@ def fill_text_center(input_text="example", fill="-", max_length=60):
     return text.center(max_length, fill)
 
 
-def fill_text_left(input_text="example", left_margin=20, fill="-", max_length=60):
+def fill_text_left(input_text="example", left_margin=15, fill="-", max_length=60):
     """Create a line with left-aligned text."""
     text = f"{(fill * left_margin)} {input_text} "
     return text.ljust(max_length, fill)
