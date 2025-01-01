d4189d3c265970d95d43cd18b7b81bb4cfc9381b
Thang Nguyen <46436648+thangckt@users.noreply.github.com>
2025-01-02 07:56:19 +0900

    u

--- thutil/io.py
+++ thutil/io.py
@@ -2,18 +2,24 @@ from pathlib import Path
 
 
 ### ANCHOR: Load data from file
-def combine_text_files(files: list[str], output_file: str):
-    """Combine text files into a single file."""
+def combine_text_files(files: list[str], output_file: str, chunk_size: int = 256):
+    """
+    Combine text files into a single file in a memory-efficient. Read and write in chunks to avoid loading large files into memory
+
+    Args:
+        files (list[str]): List of file paths to combine.
+        output_file (str): Path to the output file.
+        chunk_size (int, optional): Size of each chunk in KB to read/write. Defaults to 256 KB.
+    """
+    chunk_size_byte = chunk_size * 1024
     ### Create parent folder if not exist
     Path(output_file).parent.mkdir(parents=True, exist_ok=True)
-    ### combine text files
-    text = ""
-    for file in files:
-        with open(file) as f:
-            text += f.read()
-
-    with open(output_file, "w") as f:
-        f.write(text)
+    ### Open the output file for writing and append each file's content incrementally
+    with open(output_file, "w") as outfile:
+        for file in files:
+            with open(file, "r") as infile:
+                while chunk := infile.read(chunk_size_byte):  # Read in chunks
+                    outfile.write(chunk)
     return
 
 
